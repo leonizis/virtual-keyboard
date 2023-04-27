e7cb818a38486267f9ed9197e60bf2e3f436c62d
Leonid <leonidu36@gmail.com>
2023-04-27 07:53:44 +0300

    feat: keyboard block 3 line

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //добавляем массив с кодами клавиатуры
-let codeDown = ["IntlBackslash", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace"];
-let keyDown = ["§", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"];
+let codeDown = ["IntlBackslash", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace",
+                "Tab",'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', "Enter",
+                "CapsLock", 'KeyA', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote',
+];
+let keyDown = ["§", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
+               "Tab", 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', "Enter",
+            ,'b','a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '\\'];
 
 let container = document.createElement("div");
 container.setAttribute("class", "container");
@@ -23,14 +28,29 @@ container.appendChild(textarea);
 let keyboardBlock = document.createElement("div");
 keyboardBlock.classList.add("keyboard-block", "keyboard");
 container.appendChild(keyboardBlock);
-//добавляем кнопки
-for (let i = 0; i < keyDown.length; i++){
-    let key = document.createElement("div");
-    key.classList.add("keyboard", "key", codeDown[i]);
-    keyboardBlock.appendChild(key);
-    key.innerHTML = keyDown[i]
-    console.log(key)
+// добавляем разметку по рядам
+let keyboardKeys = [];
+let rowSizes = [14, 14, 14, 13, 10]; // колличество кнопок в каждом ряду
+
+for (let i = 0; i < 5; i++) {
+    let keyboardRow = document.createElement("div");
+    keyboardRow.classList.add("keyboard-row");
+    keyboardBlock.appendChild(keyboardRow);
+
+    let rowKeys = [];
+    let numKeys = rowSizes[i];
+    for (let j = 0; j < numKeys; j++) {
+        let key = document.createElement("div");
+        key.classList.add("key", codeDown[j + i * 14 + (i > 1 ? 1 : 0)]); // ищем индексы для строк 3-4-5
+        keyboardRow.appendChild(key);
+        key.innerHTML = keyDown[j + i * 14 + (i > 1 ? 1 : 0)];
+        rowKeys.push(key);
+    }
+
+    keyboardKeys.push(rowKeys);
 }
+
+
 
 
 
@@ -44,8 +64,8 @@ for (let i = 0; i < keyDown.length; i++){
 //
 document.onkeypress = function(event){
     console.log(event);
-    keyboard.push(event.key);
-    key.push(event.code);
-    console.log(keyboard);
-    console.log(key);
+    keyDown.push(event.key);
+    codeDown.push(event.code);
+    console.log(keyDown);
+    console.log(codeDown);
 }
